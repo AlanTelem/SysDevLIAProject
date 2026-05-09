c1e37e6a56406814acf8be1fd302744940ca1151
AlanTelem <deleteriouseuph@gmail.com>
2026-05-08 21:16:12 -0400

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Domain\Models\CardsModel;
 use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Domain\Services\TCGApiService;
+
 
 class HomeController extends BaseController
 {
     //NOTE: Passing the entire container violates the Dependency Inversion Principle and creates a service locator anti-pattern.
     // However, it is a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.
-    public function __construct(Container $container)
+    public function __construct(Container $container, private TCGApiService $tcgapi, private CardsModel $cardsModel)
     {
         parent::__construct($container);
     }
@@ -30,6 +33,17 @@ class HomeController extends BaseController
         //dd($data);
         //var_dump($this->session); exit;
         return $this->render($response, 'homeView.php', $data);
+    }
+    public function apiTest(Request $request, Response $response, array $args): Response
+    {
+        $this->cardsModel->populateOnePieceBluePrints();
+        return $this->render($response, 'homeView.php');
+    }
+
+    public function setImport(Request $request, Response $response, array $args): Response
+    {
+        $this->cardsModel->importMTGSets();
+        return $this->render($response, 'homeView.php');
     }
 
     public function error(Request $request, Response $response, array $args): Response
